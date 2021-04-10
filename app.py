6181fa6b6c329d0ac7a95db6e020551a45bbc9ca
Glorian Kosi <gk349@njit.edu>
2021-04-10 08:27:45 +0000

    sample landing page and some non-working components for login, still wip

---
+++ app.py
@@ -0,0 +1,26 @@
+import os
+from flask import Flask, send_from_directory, json, session
+from flask_socketio import SocketIO
+from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__, static_folder="./build/static")
+
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
+
+socketio = SocketIO(app, cors_allowed_origins="*", json=json, manage_session=False)
+
+@app.route("/", defaults={"filename": "index.html"})
+@app.route("/<path:filename>")
+def index(filename):
+    return send_from_directory("./build", filename)
+    
+@socketio.on("connect")
+def on_connect():
+    print("User connected!")
+
+socketio.run(
+    app,
+    host=os.getenv("IP", "0.0.0.0"),
+    port=8081 if os.getenv("C9_PORT") else int(os.getenv("PORT", 8081)),
+)
---
+++ src/Landing.js
@@ -0,0 +1,8 @@
+export function Landing(props) {
+  const socket = props.socket;
+  return (
+    <div className="wrapper">
+      <h1> Project 3 Landing Page (Needs title!!)</h1>
+    </div>
+  );
+}

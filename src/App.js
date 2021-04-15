90219cbf02a20e06c8b73f8190b2261708ca8f8f
Chirag Patel <cyp5@njit.edu>
2021-04-15 15:39:55 +0000

    Solved merge conflicts

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
   
   return (
     <div>
-      {isLoggedIn === false ? 
+      {isLoggedIn === true ? 
         (<Landing socket={socket} setIsLoggedIn={setIsLoggedIn} />)
         :
         (

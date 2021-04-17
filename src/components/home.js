499bff63f2118817a80ce1a89f9e293f50217927
Chirag Patel <cyp5@njit.edu>
2021-04-17 22:59:44 +0000

    Updated home page and added a start exploring button with direct link to about page

--- src/components/home.js
+++ src/components/home.js
@@ -1,10 +1,17 @@
 import * as React from 'react';
-
+import { Link } from "react-router-dom";
 
 export function Home() {
     
     return (
-        <h1>Welcome</h1>    
+        <div className="home">
+            <div className="homeTitle">
+                <h1>DON'T LIMIT YOUR CHALLENGES, CHALLENGE YOUR LIMITS.</h1>
+                <h4>- JERRY DUNN</h4>
+                <Link className="exploring" to="/about"> Start Exploring </Link>
+            </div>
+        </div>
+        
     );
     
 }

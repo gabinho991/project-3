887b0fbfd01ef1b8b3e3a57f1cc6d83bb5afffd0
Hemanth Velan <hv72@njit.edu>
2021-04-27 04:30:07 +0000

    testing layout

--- src/components/workoutSearch.js
+++ src/components/workoutSearch.js
@@ -1,22 +1,23 @@
 import * as React from "react";
 
-export function WorkoutSearch() {
-  return (
-    <div>
-        <h1>This feature is currently being worked on</h1>
+export function WorkoutSearch(props) {
+    return (
         <div>
-            <label for="muscle"> Choose a muscle: </label>
-            <select name="muscle" id="muscle">
-                <option hidden disabled selected value> -- select an option -- </option>
-                <option value="Abs">Abs</option>
-                <option value="Arms">Arms</option>
-                <option value="Back">Back</option>
-                <option value="Calves">Calves</option>
-                <option value="Chest">Chest</option>
-                <option value="Legs">Legs</option>
-                <option value="Shoulders">Shoulders</option>
-            </select>
+            <h1>This feature is currently being worked on</h1>
+            <div id="dropDown" className="dropDown">
+                <label for="muscle"> Choose a muscle: </label>
+                <select name="muscle" id="muscle">
+                    <option hidden disabled selected value> -- select an option -- </option>
+                    <option value="Abs">Abs</option>
+                    <option value="Arms">Arms</option>
+                    <option value="Back">Back</option>
+                    <option value="Calves">Calves</option>
+                    <option value="Chest">Chest</option>
+                    <option value="Legs">Legs</option>
+                    <option value="Shoulders">Shoulders</option>
+                </select>
+            </div>
+            
         </div>
-    </div>
     );
 }

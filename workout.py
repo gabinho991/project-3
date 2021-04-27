f0f899bffcea342722cca97c832ebe732ec628ab
Hemanth Velan <hv72@njit.edu>
2021-04-27 04:29:55 +0000

    get json file for testing

--- workout.py
+++ workout.py
@@ -4,5 +4,26 @@ import os
 
 load_dotenv(find_dotenv())
 key = os.getenv('WORKOUT_KEY')
-print(key)
+# print(key)
+BASE_URL = "https://wger.de/api/v2/exerciseinfo.json?language=2"
 
+# params = {
+#     'format': "json",
+#     "language": 2
+# }
+
+response = requests.get(BASE_URL) #, params=params)
+# print(response)
+# print(type(response))
+# print("helooooooooooooo\n")
+data = response.json()
+print(data)
+
+# def get_workout_data():
+#     """Returns the workout data from the api and we manually
+#     pick the ones for our topic"""
+    
+#     response = requests.get(BASE_URL)
+#     data = response.json()
+    
+#     print(data)

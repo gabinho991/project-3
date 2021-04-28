c7e9d9f94eed58b9493637aa7ab49d9a4becbd3d
Chirag Patel <cyp5@njit.edu>
2021-04-28 14:18:34 +0000

    Updated local

--- app.py
+++ app.py
@@ -11,7 +11,6 @@ from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import exists
 from sqlalchemy import desc
-import models
 from dotenv import load_dotenv, find_dotenv
 from functions import *
 
@@ -34,6 +33,7 @@ APP.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('DATABASE_URL')
 APP.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 DB = SQLAlchemy(APP)
+import models
 
 DB.create_all()  # likely not needed anymore
 
--- src/components/workoutSearch.js
+++ src/components/workoutSearch.js
@@ -1,5 +1,25 @@
 import * as React from "react";
 
-export function WorkoutSearch() {
-  return <h1>This feature will be available in Milestone 2</h1>;
+export function WorkoutSearch(props) {
+    
+    //window.onload
+    
+    return (
+        <div>
+            <h1>This feature is currently being worked on</h1>
+            <div id="dropDown" className="dropDown">
+                <label for="muscle"> Choose a muscle: </label>
+                <select name="muscle" id="muscle">
+                    <option hidden disabled selected value> -- select an option -- </option>
+                    <option value="Abs">Abs</option>
+                    <option value="Arms">Arms</option>
+                    <option value="Back">Back</option>
+                    <option value="Calves">Calves</option>
+                    <option value="Chest">Chest</option>
+                    <option value="Legs">Legs</option>
+                    <option value="Shoulders">Shoulders</option>
+                </select>
+            </div>
+        </div>
+    );
 }

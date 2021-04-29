a63ea3f09796789a3cd9e0dd05efa271e8af749c
gabin ntankeu <gabinho770@yahoo.fr>
2021-04-29 03:55:41 +0000

    used prettier for formatting

--- src/Navigation.js
+++ src/Navigation.js
@@ -15,82 +15,80 @@ export function NavBar(props) {
   const info = props.info;
   const post = props.post;
   const changeInfo = props.changeInfo;
-  
+
   return (
     <div className="navBarWrapper">
-      
-        <div className="NavBar">
-          <div className="Header">
-            <h1> Social Fitness </h1>
-          </div>
-          <div className="NavBtnWrap">
-            <Link id="NavButton" to="/home">
-              Home
-            </Link>
-          </div>
-          <div className="NavBtnWrap">
-            <Link id="NavButton" to="/about">
-              About
-            </Link>
-          </div>
-          <div className="NavBtnWrap">
-            <Link id="NavButton" to="/profile">
-              Profile
-            </Link>
-          </div>
-          <div className="NavBtnWrap">
-            <Link id="NavButton" to="/socialMedia">
-              Social Media
-            </Link>
-          </div>
-          <div className="NavBtnWrap">
-            <Link id="NavButton" to="/food">
-              Food search
-            </Link>
-          </div>
-          <div className="NavBtnWrap">
-            <Link id="NavButton" to="/workout">
-              Workout search
-            </Link>
-          </div>
-          <div className="NavBtnWrap">
-            <Link id="NavButton" to="/favorites">
-              Favorites
-            </Link>
-          </div>
-          <div className="NavBtnWrap" >
-            <div id="NavButton">
-              <Logout setIsLoggedIn={setIsLoggedIn} />
-            </div>
+      <div className="NavBar">
+        <div className="Header">
+          <h1> Social Fitness </h1>
+        </div>
+        <div className="NavBtnWrap">
+          <Link id="NavButton" to="/home">
+            Home
+          </Link>
+        </div>
+        <div className="NavBtnWrap">
+          <Link id="NavButton" to="/about">
+            About
+          </Link>
+        </div>
+        <div className="NavBtnWrap">
+          <Link id="NavButton" to="/profile">
+            Profile
+          </Link>
+        </div>
+        <div className="NavBtnWrap">
+          <Link id="NavButton" to="/socialMedia">
+            Social Media
+          </Link>
+        </div>
+        <div className="NavBtnWrap">
+          <Link id="NavButton" to="/food">
+            Food search
+          </Link>
+        </div>
+        <div className="NavBtnWrap">
+          <Link id="NavButton" to="/workout">
+            Workout search
+          </Link>
+        </div>
+        <div className="NavBtnWrap">
+          <Link id="NavButton" to="/favorites">
+            Favorites
+          </Link>
+        </div>
+        <div className="NavBtnWrap">
+          <div id="NavButton">
+            <Logout setIsLoggedIn={setIsLoggedIn} />
           </div>
         </div>
+      </div>
 
-        <Switch>
-          <div className="mainBody">
-            <Route path="/home">
-              <Home />
-            </Route>
-            <Route path="/about">
-              <About />
-            </Route>
-            <Route path="/profile">
-              <Profile socket={socket} info={info} changeInfo={changeInfo} />
-            </Route>
-            <Route path="/socialMedia">
-              <SocialMedia socket={socket} info={info}  post={post}/>
-            </Route>
-            <Route path="/food">
-              <FoodSearch socket={socket}/>
-            </Route>
-            <Route path="/workout">
-              <WorkoutSearch />
-            </Route>
-            <Route path="/favorites">
-              <Favorites />
-            </Route>
-          </div>
-        </Switch>
-      
+      <Switch>
+        <div className="mainBody">
+          <Route path="/home">
+            <Home />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/profile">
+            <Profile socket={socket} info={info} changeInfo={changeInfo} />
+          </Route>
+          <Route path="/socialMedia">
+            <SocialMedia socket={socket} info={info} post={post} />
+          </Route>
+          <Route path="/food">
+            <FoodSearch socket={socket} />
+          </Route>
+          <Route path="/workout">
+            <WorkoutSearch />
+          </Route>
+          <Route path="/favorites">
+            <Favorites />
+          </Route>
+        </div>
+      </Switch>
     </div>
   );
 }
--- src/components/workoutSearch.js
+++ src/components/workoutSearch.js
@@ -1,25 +1,27 @@
 import * as React from "react";
 
 export function WorkoutSearch(props) {
-    
-    //window.onload
-    
-    return (
-        <div>
-            <h1>This feature is currently being worked on</h1>
-            <div id="dropDown" className="dropDown">
-                <label for="muscle"> Choose a muscle: </label>
-                <select name="muscle" id="muscle">
-                    <option hidden disabled selected value> -- select an option -- </option>
-                    <option value="Abs">Abs</option>
-                    <option value="Arms">Arms</option>
-                    <option value="Back">Back</option>
-                    <option value="Calves">Calves</option>
-                    <option value="Chest">Chest</option>
-                    <option value="Legs">Legs</option>
-                    <option value="Shoulders">Shoulders</option>
-                </select>
-            </div>
-        </div>
-    );
+  //window.onload
+
+  return (
+    <div>
+      <h1>This feature is currently being worked on</h1>
+      <div id="dropDown" className="dropDown">
+        <label for="muscle"> Choose a muscle: </label>
+        <select name="muscle" id="muscle">
+          <option hidden disabled selected value>
+            {" "}
+            -- select an option --{" "}
+          </option>
+          <option value="Abs">Abs</option>
+          <option value="Arms">Arms</option>
+          <option value="Back">Back</option>
+          <option value="Calves">Calves</option>
+          <option value="Chest">Chest</option>
+          <option value="Legs">Legs</option>
+          <option value="Shoulders">Shoulders</option>
+        </select>
+      </div>
+    </div>
+  );
 }

fb596715a2a3b60d898eb53178217758a2a4db6a
Chirag Patel <cyp5@njit.edu>
2021-05-04 01:32:24 +0000

    Styled the Workout Search Tab

--- src/components/workoutSearch.js
+++ src/components/workoutSearch.js
@@ -2,6 +2,7 @@ import * as React from "react";
 import { useEffect } from "react";
 import { useState } from "react";
 import $ from "jquery";
+import "../workoutSearch.css";
 //force-push #2
 export function WorkoutSearch(props) {
   const socket = props.socket;
@@ -45,50 +46,61 @@ export function WorkoutSearch(props) {
   };
   return (
     <div>
-      <h1>This feature is currently being worked on</h1>
       <div id="dropDown" className="dropDown">
-        <label for="muscle"> Choose a muscle: </label>
-        <select name="muscle" id="muscle">
-          <option hidden disabled selected value>
-            {" "}
-            -- select an option --{" "}
-          </option>
-          <option value="10">Abs</option>
-          <option value="8">Arms</option>
-          <option value="12">Back</option>
-          <option value="14">Calves</option>
-          <option value="11">Chest</option>
-          <option value="9">Legs</option>
-          <option value="13">Shoulders</option>
-        </select>
-        <button id="muscleSearch" onClick={(e) => onSubmit()}>
-          Search
-        </button>
-        <div className="recipeBg">
-          <div className="recipeWrap">
+        <div className="workoutHeader">
+          <h1>This feature is currently being worked on</h1>
+          <div className="searchWrap">
+            <label for="muscle"> Choose a muscle: </label>
+            <select name="muscle" id="muscle">
+              <option hidden disabled selected value>
+                {" "}
+                -- select an option --{" "}
+              </option>
+              <option value="10">Abs</option>
+              <option value="8">Arms</option>
+              <option value="12">Back</option>
+              <option value="14">Calves</option>
+              <option value="11">Chest</option>
+              <option value="9">Legs</option>
+              <option value="13">Shoulders</option>
+            </select>
+            <button id="muscleSearch" onClick={(e) => onSubmit()}>
+              Search
+            </button>
+          </div>
+        </div>
+        <div className="workoutBg">
+          <div className="workoutWrap">
             {workouts.length !== 0
               ? workouts.map((workout) => {
                   return (
-                    <div>
-                      <div className="recipeBox">
-                        {currentWorkoutFavorites.includes(workout.name) ===
-                        false ? (
-                          <button
-                            id="fav-btn"
-                            onClick={(e) => onFavorite(workout)}
-                          >
-                            <img src="pre-fav.png" />{" "}
-                          </button>
-                        ) : (
-                          <button
-                            id="fav-btn"
-                            onClick={(e) => onRemoveFavorite(workout)}
-                          >
-                            <img src="pre-fav.png" />{" "}
-                          </button>
-                        )}
-                        <h3> {workout.name} </h3>
-                        <p> {workout.description} </p>
+                    <div className="flip-card">
+                      <div className="flip-card-inner">
+                        <div className="workoutBoxFront">
+                          <h3> {workout.name} </h3>
+                          <div className="imgContainer">
+                            <img src="https://img.etimg.com/photo/msid-74747053,quality-100/for-miles-a-great-bodyweight-workout-would-include-squats-push-ups-walking-lunges-.jpg" />
+                          </div>
+                        </div>
+                        <div className="workoutBoxBack">
+                          {currentWorkoutFavorites.includes(workout.name) ===
+                          false ? (
+                            <button
+                              id="fav-btn"
+                              onClick={(e) => onFavorite(workout)}
+                            >
+                              <img src="pre-fav.png" />{" "}
+                            </button>
+                          ) : (
+                            <button
+                              id="fav-btn"
+                              onClick={(e) => onRemoveFavorite(workout)}
+                            >
+                              <img src="pre-fav.png" />{" "}
+                            </button>
+                          )}
+                          <p> {workout.description} </p>
+                        </div>
                       </div>
                     </div>
                   );
